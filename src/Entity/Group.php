915244927595640c529ce6301b1231c7e3ad9d2b
Dmytro12344 <dmytro.vasiuk@nure.ua>
2019-08-18 02:17:42 +0300

    added native sonata auth

---
+++ src/Entity/Group.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Sonata\UserBundle\Entity\BaseGroup as BaseGroup;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="fos_group")
+ */
+class Group extends BaseGroup
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer", options={"unsigned":true})
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @return int
+     */
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+}
